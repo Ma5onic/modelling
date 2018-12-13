9150234706ede605144045706e480b8128572d7e
Rutger Ruizendaal <ruizendaalr@gmail.com>
2018-12-13 11:43:51 +0100

    added rv

--- modelling/train.py
+++ modelling/train.py
@@ -1,2 +1,13 @@
+""""
+Contains first functions for functionality testing
+"""
+
 def get_data():
+    """"
+    Prints a test
+
+    :return: nothing
+    """
     print("test: getting the data")
+
+
