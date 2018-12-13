b79ae53538ef0f221b4a084248f084d4bccf7872
Rutger Ruizendaal <ruizendaalr@gmail.com>
2018-12-13 12:07:33 +0100

    added mape

---
+++ modelling/residual_values.py
@@ -0,0 +1,11 @@
+""""
+Contains functionality specific for the RV project
+"""
+
+import numpy as np
+
+def mape(y_true, y_pred): 
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+
