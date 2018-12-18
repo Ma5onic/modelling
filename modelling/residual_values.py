86fc7d1c9f020460d798825198ea214d82aedc97
Rutger Ruizendaal <ruizendaalr@gmail.com>
2018-12-18 16:46:02 +0100

    new matrix function

--- modelling/residual_values.py
+++ modelling/residual_values.py
@@ -66,11 +66,11 @@ def duplicate_testset(testset,mileage_range,duration_range,splitting_variable,sp
 
 
 
-def duplicated_testset_to_matrix(duplicated_testset_predicted,min_matrix_size,splitting_variable,splitting_values,saving_directory):
+def duplicated_testset_to_matrix(duplicated_testset_predicted,min_matrix_size,splitting_variable,splitting_values,save_true_false,saving_directory,filename,colorcoding=False):
 
     """
     
-    Multiplies the testset into duplicate values with al the mileage, duration and splitting value combinations.
+    Transforms the duplicated testset into matrices with predicted RV percentages
     
     Parameters
     ----------
@@ -82,8 +82,14 @@ def duplicated_testset_to_matrix(duplicated_testset_predicted,min_matrix_size,sp
         The name of the variable on which the matrices should be split for all make_models
     splitting_values: list
         The values that occur in the splitting variable that should be used for splitting
+    save_true_fale: bool
+        Boolean to indicate whether matrices should be written to excel file
     saving_directory: str
-        Path to the directory in which the matrices should be saved
+        Path to the directory in which the excel file should be saved
+    filename: str
+        Name of the excel file to save
+    colorcoding: bool
+        Indicates whether colorcoding should be added to the excel matrices, default is False
     
     """
     
@@ -92,8 +98,7 @@ def duplicated_testset_to_matrix(duplicated_testset_predicted,min_matrix_size,sp
     for value in splitting_values:
         dummies.append(splitting_variable+'_'+value)
     duplicated_testset_predicted[splitting_variable] = duplicated_testset_predicted[dummies].idxmax(axis=1)
-
-    duplicated_testset_clean = duplicated_testset_predicted[['make','model_cons','mileage','duration','pred','van',splitting_variable]]
+    duplicated_testset_clean = duplicated_testset_predicted[['make','model_cons','mileage','duration','pred','van','catalog_price_car_options', splitting_variable]]
     
     # Count the size of each make_model in the testset and add this as a variable to each observation
     duplicated_testset_clean['make_model']= duplicated_testset_clean['make'] + ' ' + duplicated_testset_clean['model_cons']
@@ -103,6 +108,11 @@ def duplicated_testset_to_matrix(duplicated_testset_predicted,min_matrix_size,sp
     #Find the mileage range, duration range and splitting values used in duplication
     mileage=sorted(duplicated_testset_clean.mileage.unique())
     duration=sorted(duplicated_testset_clean.duration.unique())
+    
+    # Test correct grouping of input data
+    if len(mileage)>100 or len(duration)>100:
+        print('input mileage or duration not grouped correctly')
+        return
     
     # Compare each make_model size to the minimal matrix size and decide in which matrix observations should go
     min_matrix_size_duplicated = min_matrix_size*len(mileage)*len(duration)*len(splitting_values)
@@ -114,14 +124,21 @@ def duplicated_testset_to_matrix(duplicated_testset_predicted,min_matrix_size,sp
     duplicated_testset_clean['matrix'] = np.select(conditions, choices, default='error')
     duplicated_testset_clean['matrix'] = duplicated_testset_clean['matrix'] + ' ' + duplicated_testset_clean[splitting_variable]
     
-    # Fill in all the cells in each matrix
-    predicted_testset = duplicated_testset_clean[['mileage','duration','pred','matrix']]
+  
+    # Prepare filling of matrices
+    predicted_testset = duplicated_testset_clean[['mileage','duration','pred','matrix','catalog_price_car_options']]
     matrix_dict = {}
     listmatrices=sorted(list(predicted_testset.matrix.unique()))
+    if len(listmatrices)>100:
+        print('too many different matrices')
+        return
+    elif len(listmatrices)<5:
+        print('too little different matrices')
+        return
+    
+    # Fill in every cell in all the matrices
     for matrix in listmatrices:
         matrix_dict[matrix] = np.zeros((len(mileage)+1,len(duration)+1))
-        matrix_dict[matrix][0,1:len(duration)+1]=duration
-        matrix_dict[matrix][1:len(mileage)+1,0]=mileage
         row_index=1
         subset1 = predicted_testset[predicted_testset.matrix == matrix]
         for miles in mileage:
@@ -132,26 +149,36 @@ def duplicated_testset_to_matrix(duplicated_testset_predicted,min_matrix_size,sp
                 matrix_dict[matrix][row_index,column_index]=mean(subset3['pred'])
                 column_index += 1
             row_index += 1
+        matrix_dict[matrix]=matrix_dict[matrix]/mean(subset1['catalog_price_car_options'])
+        matrix_dict[matrix][0,1:len(duration)+1]=duration
+        matrix_dict[matrix][1:len(mileage)+1,0]=mileage
+    
     
     # Write the matrices to a single excel file, with every sheet containing one make_model
-    os.chdir(saving_directory)
-    writer=pd.ExcelWriter('RVmatricesV0.95.xlsx',engine='xlsxwriter')
-    workbook=writer.book
-    bold = workbook.add_format({'bold': True})
-    for matrix in listmatrices:
-        carname = matrix
-        split_order = 0
-        for split_value in splitting_values:
-            carname = carname.replace(split_value, '')
-            carname = carname.replace(splitting_variable, '')
-            carname = carname.replace('_', '')
-            if split_value in matrix:
-                start_row = 1 + split_order * (len(mileage) + 4)
-            split_order += 1
-        pd.DataFrame(matrix_dict[matrix]).to_excel(writer,
-                    sheet_name = carname,
-                    startrow = start_row,
-                    header = False,
-                    index = False)
-        writer.sheets[carname].write(start_row-1,0,matrix,bold)
-    writer.save()
+    if save_true_false:
+        os.chdir(saving_directory)
+        writer=pd.ExcelWriter(filename,engine='xlsxwriter')
+        workbook=writer.book
+        bold_fmt = workbook.add_format({'bold': True})
+        for matrix in listmatrices:
+            matrix_clean = matrix.replace(splitting_variable, '')
+            matrix_clean = matrix_clean.replace('_', '')
+            carname = matrix_clean
+            split_order = 0
+            for split_value in splitting_values:
+                carname = carname.replace(split_value, '')
+                if split_value in matrix_clean:
+                    start_row = 1 + split_order * (len(mileage) + 4)
+                split_order += 1
+            pd.DataFrame(matrix_dict[matrix]).to_excel(writer,
+                        sheet_name = carname,
+                        startrow = start_row,
+                        header = False,
+                        index = False)
+            writer.sheets[carname].write(start_row-1,0,matrix_clean,bold_fmt)
+            if colorcoding:
+                writer.sheets[carname].conditional_format('B'+str(start_row+2)+':'+chr(ord('B')+len(duration)-1)+str(start_row+len(mileage)+1), {'type': '3_color_scale'})
+        writer.save()
+    
+    return matrix_dict
+
